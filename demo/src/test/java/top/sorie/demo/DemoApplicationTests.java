c961d719e2f05b93210b272d5f04dcd759828a08
songzl <songzl@isvtop.com>
2021-06-23 22:11:45 +0800

    test

---
+++ demo/src/test/java/top/sorie/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.sorie.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
